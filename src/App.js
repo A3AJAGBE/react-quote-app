09642c8710aaf4960aba56e1d4c64c11a6f6633f
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2021-11-20 17:19:18 +0000

    Add app structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <Container fluid id="App-Container">
+      <div className="Quote-Div text-center">
+        <h1 className="display-4 fw-bold mb-4">A3AJAGBE Quote App</h1>
+
+        <Card id="App-Card" className="col-lg-6 mx-auto">
+          <Card.Body>
+            <blockquote className="blockquote mb-0" id="text">
+              <p className="lead mb-2 fs-3">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mauris ex, maximus vel aliquet non, venenatis a urna. Vestibulum felis sem, ullamcorper ut posuere in, consequat quis mauris.
+              </p>
+            </blockquote>
+          </Card.Body>
+
+          <Card.Title id="author"> <h3 className="mb-0">&mdash; Quote By</h3> </Card.Title>
+
+          <Card.Footer>
+              <Row className="flex-md-row-reverse g-4">
+                <Col md={6} className="d-grid d-md-flex justify-content-md-end">
+                  <a href="https://getbootstrap.com/docs/5.1/examples/heroes/" target="_blank" rel="noopener noreferrer" className="btn btn-lg coral" id="new-quote"> New </a>
+
+                  <hr />
+                </Col>
+                <Col className="d-grid gap-2 d-md-flex">
+                  <a href="https://getbootstrap.com/docs/5.1/examples/heroes/" target="_blank" rel="noopener noreferrer" className="btn btn-lg btn-info" id="tweet-quote"> Twitter </a>
+
+                  <a href="https://getbootstrap.com/docs/5.1/examples/heroes/" target="_blank" rel="noopener noreferrer" className="btn btn-lg btn-secondary">Instagram</a>
+                </Col>
+              </Row>
+          </Card.Footer>
+
+        </Card>
+      </div>
+    </Container>
   );
 }
 
